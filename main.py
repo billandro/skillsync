cf11496c1add9702a98803af2eddede35f30498f
Bill Qubeka <biqubekjhb024@student.wethinkcode.co.za>
2025-02-18 15:07:54 +0200

    Create log in, sign-up, and main functions

--- main.py
+++ main.py
@@ -3,71 +3,92 @@ from firebase_admin import credentials, db
 from firebase_admin import auth
 import json
 
-# Class to create a user instance
-class User:
-    def __init__(self, name, email, role, password):
-        self.name = name
-        self.email = email
-        self.role = role
-        self.role = password
 
-    def change_name(self, new_name):
-        self.name = new_name
+# def read_from_database(path):
+#     ref = db.reference(path)
+#     snapshot = ref.get()
 
-    def __str__(self):
-        return f"{self.name}, {self.email}, {self.role}"
+# def write_to_the_database(path):
+#     ref = db.reference(path)
+#     ref.set({
+#     })
+
+# def update_data():
+#     ref = db.reference('your_data_path')
+#     ref.update({
+#         'age': 31
+#     })
 
 
 def connect_to_database():
     cred = credentials.Certificate("skillsync-project-451208-firebase-adminsdk-fbsvc-247be52390.json")
-    firebase_admin.admin(cred, {
+    firebase_admin.initialize_app(cred, {
         "databaseURL": "https://skillsync-project-451208-default-rtdb.firebaseio.com/"
     })
 
 
-def read_from_database(path):
-    ref = db.reference(path)
-    snapshot = ref.get()
-
-
-def write_to_the_database(path):
-    ref = db.reference(path)
-    ref.set({
-    })
-
+def login():
+    print("Log in...")
+    email = input("Enter email: ")
+    password = input("Enter password: ")
+    guy = auth.get_user_by_email(email)
+    print(type(guy))
+    print(guy)
     
-def update_data():
-    ref = db.reference('your_data_path')
-    ref.update({
-        'age': 31
-    })
 
-def authentic_user(name:str, email:str, password:int):
-    # name, email, role, password
+def sign_up():
+    print("Sign up.....")
+    email = input("Enter email: ").strip()
+    password = input("Enter password: ").strip()
+    first_name = input("Enter first name: ").strip()
+     
+    # Prompt the user for a role
+    role = input("Are you signing up to be a 'MENTOR' or 'PEER'?: ").strip().lower()
+    expertise = input("What is your expertise?: ").strip().lower()
+
+    try:
+        authenticate_user(first_name, email, role, password, expertise)
+    except auth.EmailAlreadyExistsError:
+        print("Error: email already exists")
+
+
+def authenticate_user(full_name:str, email:str, role:str, password:int, expertise:str):
     user = auth.create_user(
-        display_name = name,
+        display_name = full_name,
         email = email,
         password = password,
     )
-
-    print(f"user ID: {user.uid}, password: {user.password}, name: {user.display_name}, email: {user.email}")
+    print(f"user_id: {user.uid}, full_name: {user.display_name}, email: {user.email}, role: {role}, expertise: {expertise}")
     
-    # Prompt the user for a role
-    role = input("Are you signing up to be a MENOTOR or PEER?: ").strip()
-    user.uid = User(user.name, user.email, user.role, user.password)
-    
-    ref = db.reference(f"/Users/{user.uid}")
-    user_details = {
-        "name": user.name,
-        "email": user.email,
+    # ref = db.reference(f"/Users/{user.uid}")
+    the_id = user.uid
+    the_user = {
+        "full_name": full_name,
+        "email": email,
         "role": role,
-        "password": user.password,
+        "expertise": expertise,
     }
-    file_contents = json.load(user_details)
-    print(file_contents)
-    # ref.set()
 
-print(authentic_user("example", "example.gmail.com", 0000))
+    print(f"{user.uid}")
+    # file_contents = json.load(user.uid)
+    # print(type(file_contents))
+
+
+def main():
+    # Connect to the databade first
+    connect_to_database()
+    returning_user = input("Are you a returning user?[y/n] ").strip().lower()
+
+    if returning_user == "y":
+        login()
+    elif returning_user == "n":
+        sign_up()
+
+
+main()
+
+
+# print(authentic_user("example", "example.gmail.com", 0000))
 # ref = db.reference("/Books/Best_Sellers/")
 # best_sellers = ref.get()
 # print(best_sellers)
@@ -83,11 +104,5 @@ print(authentic_user("example", "example.gmail.com", 0000))
 # with open("book_info.json", "r") as f:
 #         file_contents = json.load(f)
 #     ref.set(file_contents)
-
-
-def main():
-    # Connect to the databade first
-    connect_to_database()
-
-    #
+    
     
