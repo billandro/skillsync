2ede5eacd1026df9370d60d9db0f7fa0fb7ef183
Bill Qubeka <Bill Qubeka>
2025-02-23 18:23:27 +0200

    Remove logic from main and move it to authenticate function

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ import click
 
 
 def connect_to_database():
+    """Connect to Firebase Database"""
     cred = credentials.Certificate("skillsync-project-451208-firebase-adminsdk-fbsvc-247be52390.json")
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://skillsync-project-451208-default-rtdb.firebaseio.com/"
     })
 
 
-@click.command()
-@click.option("--returning_user", prompt="Are you a returning user?[y/n]")
-def main(returning_user):
-    # Connect to the databade first
+@click.group()
+def main():
+    """Main entry point for the CLI"""
     connect_to_database()
-    # returning_user = input("Are you a returning user?[y/n] ").strip().lower()
 
-    if returning_user == "y":
+
+@main.command()
+@click.option("--returning_user", prompt="Are you a returning user?[y/n]", type=str)
+def authenticate(returning_user):
+    """Handles user authentication based on returning status."""
+    if returning_user.lower() == "y":
         login()
-    elif returning_user == "n":
+    elif returning_user.lower() == "n":
         sign_up()
     else:
-        click.echo("You entered the invalid option '{}'".format(returning_user))
+        click.secho(f"Invalid option '{returning_user}', please enter 'y' or 'n'", fg="red")
 
 
-# main()
+if __name__ == "__main__":
+    main()
