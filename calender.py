1b5e5c73a2da14f43f65bacc29578c4d73d37ef4
Bill Qubeka <biqubekjhb024@student.wethinkcode.co.za>
2025-02-26 13:34:53 +0200

    Correct attendees of calender event

--- calender.py
+++ calender.py
@@ -6,6 +6,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from firebase_admin import db, auth
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
@@ -25,42 +26,41 @@ def get_credentials():
         with open("token.json", "w") as token:
             token.write(creds.to_json())
     
-    
-        service = build("calendar", "v3", credentials=creds)
+    return creds
 
 
+def create_event(email):
+    creds = get_credentials()
+    user = auth.get_user_by_email(email)
 
-
-
-
+    service = build("calendar", "v3", credentials=creds)
 
     event = {
-    'summary': 'Google I/O 2015',
-    'location': '800 Howard St., San Francisco, CA 94103',
-    'description': 'A chance to hear more about Google\'s developer products.',
-    'start': {
-        'dateTime': '2015-05-28T09:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    },
-    'end': {
-        'dateTime': '2015-05-28T17:00:00-07:00',
-        'timeZone': 'America/Los_Angeles',
-    },
-    'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-    ],
-    'attendees': [
-        {'email': 'lpage@example.com'},
-        {'email': 'sbrin@example.com'},
-    ],
-    'reminders': {
-        'useDefault': False,
-        'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': 10},
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2015-05-28T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2015-05-28T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
         ],
-    },
+        'attendees': [
+            {'email': user["email"]}
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+            ],
+        },
     }
 
     event = service.events().insert(calendarId='primary', body=event).execute()
-    print'Event created: %s' % (event.get('htmlLink'))
+    print('Event created: %s' % (event.get('htmlLink')))
