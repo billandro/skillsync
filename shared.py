7e7b553283fdf2c91c42f46f0653086ae2831117
Bill Qubeka <Bill Qubeka>
2025-03-03 00:34:30 +0200

    Add add_to_database and validate functions

--- shared.py
+++ shared.py
@@ -1,5 +1,17 @@
 from firebase_admin import db
+import click
 
 def read_from_database(path):
     ref = db.reference(path)
     return ref.get()
+
+
+def add_to_database(the_data, meeting_id):
+    ref = db.reference(f"/Meetings/{meeting_id}")
+    ref.set(the_data)
+
+
+def validate_not_empty(ctx, param, value):
+    if not value:
+        raise click.BadParameter("Input cannot be empty")
+    return value
