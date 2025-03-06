e04d9ec5a06d637e103cc8e6814ee630ca4da090
Bill Qubeka <Bill Qubeka>
2025-03-06 13:17:21 +0200

    Add list_workshops & list_mentors functions

--- shared.py
+++ shared.py
@@ -6,7 +6,7 @@ def read_from_database(path):
     return ref.get()
 
 
-def add_to_database(the_data, meeting_id):
+def add_meeting_to_database(the_data, meeting_id):
     ref = db.reference(f"/Meetings/{meeting_id}")
     ref.set(the_data)
 
@@ -15,3 +15,30 @@ def validate_not_empty(ctx, param, value):
     if not value:
         raise click.BadParameter("Input cannot be empty")
     return value
+
+
+def list_mentors(mentors):
+    for i in range(len(mentors)):
+        if i == 0:
+            click.secho("Available mentors:", fg="blue")
+
+        key, mentor_data = mentors[i]
+
+        if mentor_data.get("role") == "mentor":
+            click.echo(f"\nMentor {i + 1}:")
+            click.echo(f'Name: {mentor_data.get("full_name") or mentor_data.get("first_name")}')
+            click.echo(f'Email: {mentor_data.get("email")}')
+            click.echo(f'Expertise: {mentor_data.get("expertise")}')
+
+
+def list_workshops():
+    data = read_from_database("/Workshops")
+    i = 0
+    # If user is indeed signed in. List all upcoming workshops
+    if "session exists":
+        for k,v in data.items():
+            if i == 0:
+                click.secho(f"\nAvailable workshops:\n", fg="blue")
+
+            click.echo(f"Topic {i + 1} - {v['topic']}")
+            i += 1
